a86235daf8f18f90044b97e9b3cd0ee9e653e09e
Arfan Shah <arfan.shah_2023@ucentralasia.org>
2023-01-15 15:34:10 +0600

    Added new django project

--- django_projects/calc/views.py
+++ django_projects/calc/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html', {'name': 'Arfan'})
+
+def add(request):
+    value1 = int(request.POST['num1'])
+    value2 = int(request.POST['num2'])
+
+    result = value1 + value2
+
+
+    return render(request, 'result.html', {'result': result})
